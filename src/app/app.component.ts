8f3ac158de78e9fa3c633c82e793c71cfa244c68
0xpc88 <0xpc88@gmail.com>
2022-01-07 14:10:11 +0000

    ethers, connect wallet, read, mint from contract

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import  { BigNumber, ethers } from "ethers";
+import { Web3Provider } from '@ethersproject/providers';
+import { abi } from 'src/contract/contract.abi';
+
+declare global {
+  interface Window {
+      ethereum:any;
+  }
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +16,50 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'web3';
+  provider!: Web3Provider;
+  address = undefined;
+  contractProps: any = {};
+  mintResult: any;
+
+  async connectWallet() {
+    this.provider = new ethers.providers.Web3Provider(window.ethereum);
+    await this.provider.send("eth_requestAccounts", []).then((wallet => {
+      this.address = wallet;
+    }))
+  }
+
+  async contractProperties() {
+    const contract = new ethers.Contract(
+      "0xd38106468798BE7421Ba3E9b04b3Ca4930a7A863",
+      abi,
+      this.provider.getSigner()
+    )
+
+    console.log('contract', contract);
+
+    const owner = await contract['owner']();
+    this.contractProps.owner = owner;
+
+    const max_supply = await contract['MAX_SUPPLY']();
+    this.contractProps.max_supply = max_supply.toNumber();
+  }
+
+  async mint(price: string) {
+    const contract = new ethers.Contract(
+      "0xd38106468798BE7421Ba3E9b04b3Ca4930a7A863",
+      abi,
+      this.provider.getSigner()
+    );
+
+    const nft_price = BigNumber.from(ethers.utils.parseEther(price));
+
+    await contract['mint'](1, { value: nft_price }).then((result: any) => {
+      this.mintResult = result;
+    });
+
+    contract.on("Transfer", () => {
+      this.mintResult = "Successful mint fam";
+    })
+  }
+
 }
